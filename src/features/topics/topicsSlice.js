acd608f23f1834f1ba6ae628d06a466173529623
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2022-08-11 18:06:24 +0100

    Add the ability to add quiz ids to the topic it is related to. And export it

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -9,10 +9,13 @@ const topicsSlice = createSlice({
         addTopic: (state, action) => {
             state.topics[action.payload.id] = action.payload;
             state.topics[action.payload.id].quizIds = [];
+        },
+        addQuizId: (state, action) => {
+            state.topics[action.payload.topicId].push(action.payload.quizId);
         }
     }
 });
 
 export const selectTopics = state => state.topics.topics;
-export const { addTopic } = topicsSlice.actions;
+export const { addTopic, addQuizId } = topicsSlice.actions;
 export default topicsSlice.reducer;
