523ba2685a66ae2eb67710a354a443a85c52dfd4
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2022-08-11 20:40:18 +0100

    Add cards slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import topicsReducer from '../features/topics/topicsSlice';
 import quizzesSlice from "../features/quizzes/quizzesSlice";
+import cardsSlice from "../features/cards/cardsSlice";
 
 export default configureStore({
   reducer: {
     topics: topicsReducer,
-    quizzes: quizzesSlice
+    quizzes: quizzesSlice,
+    cards: cardsSlice
   },
 });
---
+++ src/features/cards/cardsSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cardsSlice = createSlice({
+    name: 'cards',
+    initialState: {
+        cards: {}
+    },
+    reducers: {
+        addCard: (state, action) => {
+            state.cards[action.payload.id] = action.payload;
+        }
+    }
+});
+
+export const selectCards = state.cards.cards;
+export const { addCard } = cardsSlice.actions;
+export default cardsSlice.reducer;
